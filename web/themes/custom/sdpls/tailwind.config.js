6b140313287561bdf1f1c3a84452682f9df8d99c
Khatuna Chitishvili <chitishvili.kh@gmail.com>
2023-06-08 12:04:47 +0400

    Add custom SDPLS theme

---
+++ web/themes/custom/sdpls/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "../../../modules/custom/**/*.js",
+    "./js/**/*.js",
+    "./**/*.twig",
+    "../../../modules/custom/**/*.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
