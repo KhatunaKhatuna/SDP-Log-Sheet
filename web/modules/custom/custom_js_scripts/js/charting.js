dc9c3a646c22c05184bdb6d0a321ba2a1e2ae022
Khatuna Chitishvili <chitishvili.kh@gmail.com>
2023-08-20 22:31:57 +0400

    Add Chart creation fieldset to charting Change missing items field type from field element to fieldset in AIMdb

---
+++ web/modules/custom/custom_js_scripts/js/charting.js
@@ -0,0 +1,41 @@
+(function ($) {
+    Drupal.behaviors.sdplsWebformBehaviorcharting = {
+      attach: function (context, settings) {
+        //Change Other Element style when fieldset
+        function changeOtherElementstyleFieldset(elementId, a, b) {
+          let parentFieldset = document.getElementById(elementId);
+          let elementFieldset = parentFieldset.lastElementChild;
+          let elementOther = elementFieldset.lastElementChild;
+          if (elementOther) {
+            elementOther.style.margin = "0";
+            elementOther.style.width = "100%";
+            elementOther.style[a] = b;
+          }
+        }
+  
+        // Call the function once on page load to apply the style initially
+        changeOtherElementstyleFieldset("edit-missing-items-charting");
+        changeOtherElementstyleFieldset(
+          "edit-data-correction-is-required-charting"
+        );
+        changeOtherElementstyleFieldset("edit-contact-with-data-originator-needed-charting");
+
+        // Function to handle window resize
+        function handleWindowResize() {
+          // Call your changeOtherElementstyle function here with the elementId
+          changeOtherElementstyleFieldset(
+            "edit-missing-items-charting"
+          );
+          changeOtherElementstyleFieldset(
+            "edit-data-correction-is-required-charting"
+          );
+          changeOtherElementstyleFieldset("edit-contact-with-data-originator-needed-charting");
+
+        }
+  
+        // Add an event listener for window resize
+        window.addEventListener("resize", handleWindowResize);
+      },
+    };
+  })(jQuery);
+  
