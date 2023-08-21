fc3050abaf9dfaf7f1daf1d7eee2118dd2f7bb28
Khatuna Chitishvili <chitishvili.kh@gmail.com>
2023-08-22 00:29:57 +0400

    Delete unnecessary function and modify sdo-db fields

--- web/modules/custom/custom_js_scripts/js/contact-with-data-originator.js
+++ web/modules/custom/custom_js_scripts/js/contact-with-data-originator.js
@@ -1,19 +1,6 @@
 (function ($) {
     Drupal.behaviors.sdplsWebformBehaviorcwdo = {
-      attach: function (context, settings) {
-        //Change Other Element style
-        function changeOtherElementstyle(elementId, a, b) {
-          let parent = document.getElementById(elementId);
-          let element = parent.lastElementChild;
-          if (element) {
-            element.style.margin = "0";
-            element.style.width = "100%";
-            element.style[a] = b;
-          }
-        }
-  
-        // Call the function once on page load to apply the style initially
-  
+      attach: function (context, settings) {  
         //Change Other Element style when fieldset
         function changeOtherElementstyleFieldset(elementId, a, b) {
           let parentFieldset = document.getElementById(elementId);
